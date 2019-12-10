a44958ebaa41ae827d293805fc34a4109a87ecd6
Nate Adams <psiberknetic@gmail.com>
2019-12-10 09:32:36 -0600

    fixup! fixup! Implement simple Legacy Contact

---
+++ InternalLegacySystem/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace InternalLegacySystem
+{
+    public class Contact
+    {
+        public Name ContactName { get; set; }
+        public string WorkPhone { get; set; }
+        public string HomePhone { get; set; }
+        public Address HomeAddress { get; set; }
+        public Address WorkAddress { get; set; }
+        public string Email { get; set; }
+    }
+}

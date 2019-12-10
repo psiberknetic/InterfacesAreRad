3b0a444015cf4e4625b7be43cd76f0076cd76063
Nate Adams <psiberknetic@gmail.com>
2019-12-10 10:24:24 -0600

    Creating initial view

---
+++ ContactManagementSystem/Controllers/ContactsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagementSystem.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public ContactsController()
+        {
+
+        }
+
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Contact/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
